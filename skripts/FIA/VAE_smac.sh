95e941a434db9f19b8ccb1474772832e794b55a7
JosuaCarl <josi.carl@gmx.de>
2024-03-26 13:23:01 +0100

    Corrected path VAE_smac.sh

--- skripts/FIA/VAE_smac.sh
+++ skripts/FIA/VAE_smac.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-python VAE_smac.py -d "../../runs/FIA/comm8/oms" -r "../../runs/VAE/hyperband_optimization"
+python VAE_smac.py -d "../../runs/FIA/Com8_grown_together/oms" -r "../../runs/VAE/hyperband_optimization"
 echo "Done"

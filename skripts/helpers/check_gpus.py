b3579e654bf497b517345d568478f916ee4d6811
JosuaCarl <josi.carl@gmx.de>
2024-04-15 10:57:24 +0200

    GPU checking

--- skripts/helpers/check_gpus.py
+++ skripts/helpers/check_gpus.py
@@ -1,18 +1,39 @@
 #!/usr/bin/env python3
 #SBATCH --job-name Test_GPU
-#SBATCH --partition gpu-a30
-#SBATCH --nodes 4
+#SBATCH --nodes 1
 #SBATCH --ntasks-per-node 1
 #SBATCH --cpus-per-task 1
-#SBATCH --gres gpu:1,gpu:2
 
+import os
+import argparse
 from GPUtil import showUtilization, getAvailable
+import torch
+import tensorflow as tf
 
 print(f"Available GPUs: {getAvailable(limit=10)}")
 showUtilization(all=True)
 
-import torch
-import tensorflow as tf
-
 print("Torch found: ", torch.cuda.is_available())
 print("Tensorflow found: ", tf.config.list_physical_devices())
+
+parser = argparse.ArgumentParser(prog='VAE_smac_run',
+                                     description='Hyperparameter tuning for Variational Autoencoder with SMAC')
+parser.add_argument('-b', '--backend', nargs=1, required=True)
+args = parser.parse_args()
+
+os.environ["KERAS_BACKEND"] = args.backend
+import keras
+import numpy as np
+
+data = np.random.rand(20,100)
+target = np.random.randint(0, 2, size=20)
+
+
+model = keras.Sequential([keras.layers.Input(shape=(data.shape[1],)),
+                          keras.layers.Dense(units=5, activation="selu"),
+                          keras.layers.Dense(units=1, activation="sigmoid")])
+
+model.compile(optimizer="nadam", loss=keras.losses.binary_crossentropy, metrics=["accuracy"])
+history = model.fit(data, target, verbose=2, epochs=5)
+print("After training:")
+showUtilization(all=True)

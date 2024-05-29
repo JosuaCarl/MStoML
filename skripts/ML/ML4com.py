cf5b5eb3ad03421f982de7ff9f3a64f10353c92c
JosuaCarl <josua.carl@student.uni-tuebingen.de>
2024-05-29 15:13:25 +0200

    Tested ML implementations

--- skripts/ML/ML4com.py
+++ skripts/ML/ML4com.py
@@ -1,14 +1,17 @@
 # imports
 import os
+import gc
 import time
 import warnings
-import math
 import itertools
 from typing import Union
 from tqdm import tqdm
 from typing import List
+from pathlib import Path
 import numpy as np
 import pandas as pd
+
+import sklearn
 from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score, roc_curve
 from sklearn.model_selection import cross_val_predict
 from sklearn.utils.random import sample_without_replacement
@@ -16,13 +19,23 @@ from sklearn.model_selection import KFold, StratifiedKFold, cross_val_score
 from sklearn.preprocessing import StandardScaler, MaxAbsScaler, MinMaxScaler
 from sklearn.model_selection import GridSearchCV, StratifiedShuffleSplit
 from sklearn import tree
-from sklearn import svm
+
 import seaborn as sns
 import matplotlib.pyplot as plt
-from ConfigSpace import Configuration, ConfigurationSpace
-from smac import HyperparameterOptimizationFacade, Scenario
+
 import mlflow
 from pathlib import Path
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['MALLOC_TRIM_THRESHOLD_'] = '0'
+
+import tensorflow as tf
+import keras
+from keras import layers, activations, backend
+
+from ConfigSpace import Categorical, Configuration, ConfigurationSpace, EqualsCondition, Float, InCondition, Integer, Constant
+from smac import HyperparameterOptimizationFacade, MultiFidelityFacade, Scenario
+from smac.intensifier.hyperband import Hyperband
 
 
 # Combination of pos/neg 
@@ -30,7 +43,10 @@ def join_df_metNames(df, include_mass=False):
     """
     Combines positively and negatively charged dataframes along their metabolite Names
     """
-    cols = ["metNames", "ref_mass"] + [f"MS{i+1}" for i in range(len(df.columns) - 6)]
+    if include_mass:
+        cols = ["metNames", "ref_mass"] + [f"MS{i+1}" for i in range(len(df.columns) - 6)]
+    else:
+        cols = ["metNames"] + [f"MS{i+1}" for i in range(len(df.columns) - 6)]
     comb = pd.DataFrame(columns=cols)
     for pid in df["peakID"].unique():
         comb_met_name = ""
@@ -46,9 +62,13 @@ def join_df_metNames(df, include_mass=False):
 
 # Helpers
 def dict_permutations(dictionary:dict) -> List[dict]:
+  """
+  Combine all value combinations in a dictionary into a list of dictionaries.
+  """
   keys, values = zip(*dictionary.items())
   return [dict(zip(keys, v)) for v in itertools.product(*values)]
 
+'''
 # SKLEARN
 def mult_cv_model(model, X, ys, n_fold):
     """
@@ -81,9 +101,11 @@ def mult_cv_model(model, X, ys, n_fold):
 
 
 def grid_search_params_cv_model(classifier, param_grid, X, ys, targets, n_splits:int=5, n_jobs:int=1):
+    """
+    #Perform Grid parameter search for a 
+    """
     grids = {}
     for i, y in enumerate(ys.transpose()):
-        # Model definition
         cv = StratifiedShuffleSplit(n_splits=n_splits, test_size=1/n_splits, random_state=42)
         grid = GridSearchCV(classifier(), param_grid=param_grid, cv=cv, n_jobs=n_jobs, verbose=2)
         grid.fit(X, y)
@@ -114,10 +136,13 @@ def train_cv_model(classifier, param_grid, X, ys, target_labels, outdir:str, suf
 
     results.to_csv(os.path.join(outdir, f"accuracies_{suffix}.tsv"), sep="\t")
     return results
+'''
 
-
-## SMAC
+## SKLearn
 class SKL_Classifier:
+    """
+    Representation of Scikit-learn classifier for SMAC3
+    """
     def __init__(self, X, ys, cv:int, configuration_space:ConfigurationSpace, classifier):
         self.X = X
         self.ys = ys
@@ -129,13 +154,13 @@ class SKL_Classifier:
     def train(self, config: Configuration, seed:int=0) -> np.float64:
         with mlflow.start_run(run_name=f"run_{self.count}", nested=True):
             mlflow.set_tag("test_identifier", f"child_{self.count}")
-            splitter = KFold(n_splits=self.cv, shuffle=True, random_state=seed)
+            splitter = StratifiedKFold(n_splits=self.cv, shuffle=True, random_state=seed)
             scores = []
             for train, test in splitter.split(self.X, self.ys):
-                model = self.classifier(**config, random_state=seed)
-                model.fit(self.X.loc[train], self.ys.loc[train])
-                y_pred = model.predict(self.X.loc[test])
-                scores.append( np.mean( y_pred == self.ys.loc[test].values) )
+                model = self.classifier(**config)
+                model.fit(self.X.iloc[train], self.ys.iloc[train])
+                y_pred = model.predict(self.X.iloc[test])
+                scores.append( np.mean( y_pred == self.ys.iloc[test].values) )
             score = np.mean( scores )
             mlflow.log_params( config )
             mlflow.log_metrics( {"accuracy": score} )
@@ -144,12 +169,15 @@ class SKL_Classifier:
 
 
 def tune_classifier(X, y, classifier, cv, configuration_space, n_trials, name, algorithm_name, outdir, verbosity):
+    """
+    Perform hyperparameter tuning on an Sklearn classifier.
+    """
     classifier = SKL_Classifier(X, y, cv=cv, configuration_space=configuration_space, classifier=classifier)
 
     scenario = Scenario( classifier.configuration_space, deterministic=True, 
-                        n_workers=1, n_trials=n_trials,
-                        walltime_limit=np.inf, cputime_limit=np.inf, trial_memory_limit=None,
-                        output_directory=outdir )
+                         n_workers=1, n_trials=n_trials,
+                         walltime_limit=np.inf, cputime_limit=np.inf, trial_memory_limit=None,
+                         output_directory=outdir )
 
     facade = HyperparameterOptimizationFacade(scenario, classifier.train, overwrite=True, logging_level=30-verbosity*10)
 
@@ -164,6 +192,9 @@ def tune_classifier(X, y, classifier, cv, configuration_space, n_trials, name, a
 
 def extract_metrics(true_labels, prediction, run_label=None, cv_i=None,
                     metrics_df:pd.DataFrame=pd.DataFrame(columns=["Run", "Cross-Validation run", "Accuracy", "AUC", "TPR", "FPR", "Threshold", "Conf_Mat"])):
+    """
+    Extract meaningful metrics to score a model according to its label prediction in comparison to true labels.
+    """
     fpr, tpr, threshold = roc_curve(true_labels,  prediction)
     auc = roc_auc_score(true_labels,  prediction)
     conf_mat = confusion_matrix(true_labels,  prediction)
@@ -179,7 +210,9 @@ def extract_metrics(true_labels, prediction, run_label=None, cv_i=None,
     return metrics_df
 
 
-def nested_cross_validate_model_sklearn(model_in, X, ys, labels, classifier, configuration_space, n_trials, name, algorithm_name, outdir, fold:Union[KFold, StratifiedKFold]=KFold(), verbosity=0):
+def nested_cross_validate_model_sklearn(X, ys, labels, classifier, configuration_space, n_trials,
+                                        name, algorithm_name, outdir, fold:Union[KFold, StratifiedKFold]=KFold(),
+                                        inner_fold:int=3, verbosity=0):
     """
     Cross-validate a model against the given hyperparameters for all organisms
     """
@@ -188,42 +221,50 @@ def nested_cross_validate_model_sklearn(model_in, X, ys, labels, classifier, con
     overall_metrics_df = pd.DataFrame(columns=["Accuracy", "AUC", "TPR", "FPR", "Threshold", "Conf_Mat"])
 
     all_predictions = np.ndarray((0))
+
+    # Iterate over all organisms for binary distinction
     for i, y in enumerate(tqdm(ys.columns)):
         y = ys[y]
         predictions = np.ndarray((0))
+
+        # Outer Loop
         for cv_i, (train_index, val_index) in enumerate(fold.split(X, y)):
             training_data = X.iloc[train_index]
             training_labels = y.iloc[train_index]
             validation_data = X.iloc[val_index]
             validation_labels = y.iloc[val_index]
 
-            incumbent = tune_classifier(training_data, training_labels, classifier, 3, configuration_space, n_trials, name, algorithm_name, outdir, verbosity)
+            # Hyperparameter Tuning with inner CV loop
+            incumbent = tune_classifier(training_data, training_labels, classifier, inner_fold, configuration_space, n_trials,
+                                        name, algorithm_name, outdir, verbosity)
+            
+            # Model definition and fitting
             if isinstance(incumbent, list):
                 best_hp = incumbent[0]
             else: 
                 best_hp = incumbent
-            model = model_in(**best_hp)		# Ensures model resetting for each cross-validation
+
+            model = classifier(**best_hp)		# Ensures model resetting for each cross-validation
 
             model.fit(np.array(training_data), np.array(training_labels))
 
+            # Prediction and scoring
             prediction = model.predict(np.array(validation_data))
             metrics_df = extract_metrics(validation_labels, prediction, labels[i], cv_i+1, metrics_df)
 			
             if verbosity != 0:
-                model.evaluate(validation_data,  validation_labels, verbose=verbosity) # type: ignore
+                if hasattr(model, "evaluate"):
+                    model.evaluate(validation_data,  validation_labels, verbose=verbosity)
+                if hasattr(model, "score"):
+                    print(f"Mean accuracy: {model.score(validation_data,  validation_labels)}")
 
             predictions = np.append(predictions, prediction)
         organism_metrics_df = extract_metrics(y, predictions, labels[i], metrics_df=organism_metrics_df)
         all_predictions = np.append(all_predictions, predictions)
 
     overall_metrics_df = extract_metrics(ys.to_numpy().flatten(), all_predictions, metrics_df=overall_metrics_df)
-    return (metrics_df, organism_metrics_df, overall_metrics_df)
 
-
-def cross_validate_classifier(X, ys, targets, classifier, configuration_space, n_trials, name, algorithm_name, outdir, verbosity):
-    metrics_df, organism_metrics_df, overall_metrics_df = nested_cross_validate_model_sklearn( classifier, X, ys, targets, classifier,
-                                                                                               configuration_space, n_trials, name,
-                                                                                               fold=StratifiedKFold(n_splits=5), verbosity=verbosity)
+    # Saving of results
     metrics_df.to_csv(os.path.join(outdir, f"{algorithm_name}_metrics.tsv"), sep="\t")
     organism_metrics_df.to_csv(os.path.join(outdir, f"{algorithm_name}_organism_metrics.tsv"), sep="\t")
     overall_metrics_df.to_csv(os.path.join(outdir, f"{algorithm_name}_overall_metrics.tsv"), sep="\t")
@@ -231,6 +272,133 @@ def cross_validate_classifier(X, ys, targets, classifier, configuration_space, n
     return (metrics_df, organism_metrics_df, overall_metrics_df)
    
 
+# Keras
+def build_classification_model(config:Configuration, classes:int=1):
+    backend.clear_session()
+    gc.collect()
+    # Model definition
+    model = keras.Sequential(name="MS_community_classifier")
+    model.add( keras.layers.Dropout( config["dropout_in"] ) )
+    model.add( keras.layers.BatchNormalization() )
+    for i in range( config["n_layers"] ):
+        activation = config[f"activation_{i}"]
+        if activation == "leakyrelu":
+            activation = layers.LeakyReLU()
+        model.add( layers.Dense( units=config[f"n_neurons_{i}"], activation=activation)  )
+        if config[f"dropout_{i}"]:
+            model.add(keras.layers.Dropout(0.5, noise_shape=None, seed=None))
+        model.add(keras.layers.BatchNormalization())
+
+    model.add(layers.Dense(classes,  activation=activations.sigmoid))
+    if classes == 1:
+        loss_function = keras.losses.BinaryCrossentropy()
+    else:
+        loss_function = keras.losses.CategoricalCrossentropy()
+
+    if config["solver"] == "nadam":
+        optimizer = keras.optimizers.Nadam( learning_rate=config["learning_rate"] )
+    
+    model.compile(optimizer=optimizer, loss=loss_function, metrics=['accuracy'])
+
+    return model
+
+class Keras_Classifier:
+    def __init__(self, X, ys, cv, configuration_space:ConfigurationSpace, model_builder, model_args):
+        self.configuration_space = configuration_space
+        self.model_builder = model_builder
+        self.model_args = model_args
+        self.fold = KFold(n_splits=cv)
+        self.X = X
+        self.ys = ys
+
+    def train(self, config: Configuration, seed: int = 0, budget: int = 25) -> np.float64:
+        model = self.model_builder(config=config, **self.model_args)
+        losses = []
+        for train_index, val_index in self.fold.split(self.X, self.ys):
+            training_data = self.X.iloc[train_index]
+            training_labels = self.ys.iloc[train_index]
+            validation_data = self.X.iloc[val_index]
+            validation_labels = self.ys.iloc[val_index]
+
+            for y in training_labels.columns:
+                keras.utils.set_random_seed(seed)
+        
+                y_train = training_labels[y]
+                y_test= validation_labels[y]
+                
+                callback = keras.callbacks.EarlyStopping(monitor='loss', patience=100)
+                model.fit(training_data, y_train, epochs=int(budget), verbose=0, callbacks=[callback])
+
+                val_loss, val_acc = model.evaluate(validation_data,  y_test, verbose=0)
+                losses.append(val_loss)
+                keras.backend.clear_session()
+
+        return np.mean(losses)
+
+
+# Evaluation
+def nested_cross_validate_model_keras(X, ys, labels, configuration_space, classes=1, fold:Union[KFold, StratifiedKFold]=KFold(),
+                                      patience:int=100, epochs:int=1000, outdir=".", verbosity=0):
+    """
+    Perform nested cross-validation with hyperparameter search on the given configuration space and subsequent evaluation
+    """
+    metrics_df = pd.DataFrame(columns=["Organism", "Cross-Validation run", "Accuracy", "AUC", "TPR", "FPR", "Threshold", "Conf_Mat"])
+
+    organism_metrics_df = pd.DataFrame(columns=["Organism", "Accuracy", "AUC", "TPR", "FPR", "Threshold", "Conf_Mat"])
+    overall_metrics_df = pd.DataFrame(columns=["Accuracy", "AUC", "TPR", "FPR", "Threshold", "Conf_Mat"])
+
+    all_predictions = np.ndarray((0))
+    for i, y in enumerate(tqdm(ys.columns)):
+        y = ys[y]
+        for cv_i, (train_index, val_index) in enumerate(fold.split(X, y)):
+            training_data = X.iloc[train_index]
+            training_labels = y.iloc[train_index]
+            validation_data = X.iloc[val_index]
+            validation_labels = y.iloc[val_index]
+
+            classifier = Keras_Classifier(training_data, ys, cv=3 , configuration_space=configuration_space,
+                                    model_builder=build_classification_model, model_args={"classes": 1})
+
+            scenario = Scenario( classifier.configuration_space, n_trials=1000,
+                                deterministic=True,
+                                min_budget=5, max_budget=1000,
+                                n_workers=1, output_directory=outdir,
+                                walltime_limit=12*60*60, cputime_limit=np.inf, trial_memory_limit=int(6e10)    # Max RAM in Bytes (not MB) 3600 = 1h
+                                )
+
+            initial_design = MultiFidelityFacade.get_initial_design(scenario, n_configs=100)
+            intensifier = Hyperband(scenario, incumbent_selection="highest_budget")
+            facade = MultiFidelityFacade( scenario, classifier.train, 
+                                        initial_design=initial_design, intensifier=intensifier,
+                                        overwrite=True, logging_level=20
+                                        )
+
+            incumbent = facade.optimize()
+
+            if isinstance(incumbent, list):
+                best_hp = incumbent[0]
+            else: 
+                best_hp = incumbent
+
+            model = build_classification_model(best_hp, classes)		# Ensures model resetting for each cross-validation
+
+            callback = keras.callbacks.EarlyStopping(monitor='loss', patience=patience)
+            model.fit(training_data, training_labels, epochs=epochs, verbose=0, callbacks=[callback]) # type: ignore
+
+            prediction = model.predict(validation_data)
+            metrics_df = extract_metrics(validation_labels, prediction, labels[i], cv_i+1, metrics_df)
+			
+            if verbosity != 0:
+                model.evaluate(validation_data,  validation_labels, verbose=verbosity) # type: ignore
+
+            predictions = np.append(predictions, prediction)
+            keras.backend.clear_session()
+            
+        organism_metrics_df = extract_metrics(y, predictions, labels[i], metrics_df=organism_metrics_df)
+        all_predictions = np.append(all_predictions, predictions)
+
+    overall_metrics_df = extract_metrics(ys.to_numpy().flatten(), all_predictions, metrics_df=overall_metrics_df)
+    return (metrics_df, organism_metrics_df, overall_metrics_df)
 
 # PLOTTING
 def plot_cv_confmat(ys, target_labels, accuracies, confusion_matrices, outdir, name):
@@ -263,6 +431,9 @@ def plot_decision_trees(model, feature_names, class_names, outdir, name):
 
 
 def plot_metrics_df(metrics_df, organism_metrics_df, overall_metrics_df, algorithm_name, outdir):
+    """
+    Plot the extracted metrics
+    """
     ax = sns.heatmap(metrics_df.pivot(index="Organism", columns="Cross-Validation run", values="Accuracy"),
                     vmin=0, vmax=1.0, annot=True, cmap=sns.diverging_palette(328.87,  221.63, center="light", as_cmap=True))
     fig = ax.get_figure()

9a614b3e8d54cebcdc88ec36820f66b9d99db444
JosuaCarl <josi.carl@gmx.de>
2024-07-29 18:09:02 +0200

    Added sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../skripts"))
+sys.path.insert(0, os.path.abspath(".."))
 
 project = 'Community composition VAE + Inference'
 copyright = '2024, Josua Carl'

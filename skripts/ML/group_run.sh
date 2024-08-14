7126d87729d3556e870d6f7212056cc50a2348a8
JosuaCarl <josi.carl@gmx.de>
2024-08-14 12:22:43 +0200

    EC Evaluation on GT

--- skripts/ML/group_run.sh
+++ skripts/ML/group_run.sh
@@ -3,9 +3,9 @@
 Run multiple groups of Machine Learning Tuning, Training or Evaluation experiments on a slurm cluster.
 '
 
-partition="cpu3-long"
-time="14-00:00:00"
-declare -a algorithms=( "Extreme gradient boosting RF" ) # "Decision tree" "Gaussian Naive-Bayes" "K-neighbours classifier" "Linear Discriminant Analysis" "Logistic Regression" "Neural Network (MLP) SK-learn" "Random forest" "Support-vector classifier" )
+partition="cpu1"
+time="1-00:00:00"
+declare -a algorithms=( "Decision tree" "Gaussian Naive-Bayes" "K-neighbours classifier" "Logistic Regression" "Neural Network (MLP) SK-learn" "Random forest" "Support-vector classifier" "Extreme gradient boosting RF" "Linear Discriminant Analysis" ) # 
 base_out="../../runs/ML/ml_"
 tries=200
 inner_fold=5
@@ -14,7 +14,7 @@ workers=1
 backend="tensorflow"
 source_dir="../.."
 suffix=""
-task="tune"
+task="evaluate"
 
 : '
 # Com8 equal comb
@@ -23,7 +23,7 @@ model_source="Com8_grown_together"
 file="20240702_FIA-Data_com8_equal_comb_mimedb_raw.xlsx"
 model_file="FIA-Data Com8_20230717_P0024_msAV206-312.xlsx"
 
-sbatch --partition=$partition --cpus-per-task=1 --ntasks=$workers --time=$time --output="${base_out}ec_annot${task}${suffix}.out" \
+sbatch --partition=$partition --cpus-per-task=1 --ntasks-per-node=$workers --time=$time --output="${base_out}ec_annot${task}${suffix}.out" \
     ML4com_run.py \
     -t $tries -if $inner_fold -of $outer_fold -v 0 -st $source_dir -s "annotated" -sam $source -ms $model_source \
     -b $backend -c "gpu" -n "cosine_2" -fn "${file}" -mfn "${model_file}" -a "${algorithms[@]}" -ta $task -w $workers
@@ -77,7 +77,6 @@ sbatch --partition=$partition --time=$time --output="${base_out}gt_cos${task}${s
     -t $tries -if $inner_fold -of $outer_fold -v 0 -st $source_dir -s "latent" -sam $source -ms $model_source \
     -b $backend -c "gpu" -n "cosine_2" -fn "${file}" -mfn "${model_file}" -a "${algorithms[@]}" -ta $task -w $workers
 
-: '
 sbatch --partition=$partition --time=$time --output="${base_out}gt_maecos${task}${suffix}.out" \
     ML4com_run.py \
     -t $tries -if $inner_fold -of $outer_fold -v 0 -st $source_dir -s "latent" -sam $source -ms $model_source \
@@ -87,7 +86,7 @@ sbatch --partition=$partition --time=$time --output="${base_out}gt_se${task}${su
     ML4com_run.py \
     -t $tries -if $inner_fold -of $outer_fold -v 0 -st $source_dir -s "latent" -sam $source -ms $model_source \
     -b $backend -c "cpu" -n "se_2" -fn "${file}" -mfn "${model_file}" -a "${algorithms[@]}" -ta $task -w $workers
-'
+
 sbatch --partition=$partition --time=$time --output="${base_out}gt_mae${task}${suffix}.out" \
     ML4com_run.py \
     -t $tries -if $inner_fold -of $outer_fold -v 0 -st $source_dir -s "latent" -sam $source -ms $model_source \
@@ -97,9 +96,8 @@ sbatch --partition=$partition --time=$time --output="${base_out}gt_aecos${task}$
     ML4com_run.py \
     -t $tries -if $inner_fold -of $outer_fold -v 0 -st $source_dir -s "latent" -sam $source -ms $model_source \
     -b $backend -c "cpu" -n "ae+cosine_2" -fn "${file}" -mfn "${model_file}" -a "${algorithms[@]}" -ta $task -w $workers
-: '
+
 sbatch --partition=$partition --time=$time --output="${base_out}gt_mse${task}${suffix}.out" \
     ML4com_run.py \
     -t $tries -if $inner_fold -of $outer_fold -v 0 -st $source_dir -s "latent" -sam $source -ms $model_source \
     -b $backend -c "cpu" -n "mse_2" -fn "${file}" -mfn "${model_file}" -a "${algorithms[@]}" -ta $task -w $workers
-'
